508c0a0d900234d0d2a6253966c36c25dc219c0b
leofigy <angel.fig@yandex.com>
2022-11-30 11:27:49 -0600

    click pal

--- gui/fs/main.cpp
+++ gui/fs/main.cpp
@@ -1,18 +1,35 @@
 #include <gtk/gtk.h>
 
 
+
+static void click_cb(GtkButton *btn, gpointer user_data){
+    g_print ("Clicked.\n");
+}
+
+
 static void app_activate(GApplication *app, gpointer *user_data){
     g_print("Gtk app is activated\n");
     GtkWidget *win;
-    win = gtk_window_new();
+    GtkWidget *btn;
+
+    win = gtk_application_window_new(
+      GTK_APPLICATION(app)
+    );
 
     gtk_window_set_title (GTK_WINDOW (win), "Wizard");
     gtk_window_set_default_size(GTK_WINDOW(win), 600, 600);
 
-    gtk_window_set_application(
-        GTK_WINDOW(win),
-        GTK_APPLICATION(app)
+
+    btn = gtk_button_new_with_label("Hello pal");
+    gtk_window_set_child(GTK_WINDOW(win), btn);
+
+    g_signal_connect(
+      btn,
+      "clicked",
+      G_CALLBACK(click_cb),
+      NULL
     );
+
     gtk_widget_show(win);
 }
 
