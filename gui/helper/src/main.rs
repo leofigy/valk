e9a6e7a1db91cc48aa7f163c8b6028ea72100c2d
leofigy <angel.fig@yandex.com>
2023-01-17 00:33:07 -0600

    example ongoing

--- gui/helper/src/main.rs
+++ gui/helper/src/main.rs
@@ -1,5 +1,8 @@
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Button};
+use gtk::{self, glib, Application, ApplicationWindow, Button, Orientation, Box};
+use glib::clone;
+use std::rc::Rc;
+use std::cell::Cell;
 
 const APP_ID: &str =  "xyz.leofigy.helper";
 
@@ -13,8 +16,16 @@ fn main() {
 fn build_ui(app: &Application){
 
     // pieces
-    let button = Button::builder()
-        .label("Click on me")
+    let button_increase = Button::builder()
+        .label("increase")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    let button_decrease = Button::builder()
+        .label("decrease")
         .margin_top(12)
         .margin_bottom(12)
         .margin_start(12)
@@ -22,17 +33,30 @@ fn build_ui(app: &Application){
         .build();
 
     
-    button.connect_clicked(
-        |button| {
-            button.set_label("Welcome|Wilkommen|Bienvenido");
-        }
-    );
+    let number = Rc::new(Cell::new(0));
 
+    button_increase.connect_clicked(clone!(@weak number, @weak button_decrease =>
+        move |_| {
+            number.set(number.get() + 1);
+            button_decrease.set_label(&number.get().to_string());
+    }));
+    button_decrease.connect_clicked(clone!(@weak button_increase =>
+        move |_| {
+            number.set(number.get() - 1);
+            button_increase.set_label(&number.get().to_string());
+    }));
+
+    let boxed = Box::builder()
+    .orientation(Orientation::Vertical)
+    .build();
+
+    boxed.append(&button_increase);
+    boxed.append(&button_decrease);
 
     let window = ApplicationWindow::builder()
     .application(app)
     .title("Helper app")
-    .child(&button)
+    .child(&boxed)
     .build();
 
     window.present();
