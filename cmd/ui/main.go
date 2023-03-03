e30c6edf1dd4b9f3e4edd32a9896cb7943516c20
leofigy <angel.figu@yandex.com>
2023-03-03 17:11:20 -0600

    pg

--- cmd/ui/main.go
+++ cmd/ui/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
@@ -8,9 +12,9 @@ import (
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello")
+	w := a.NewWindow("Clock")
 
-	hello := widget.NewLabel("assistant ...")
+	hello := widget.NewLabel("")
 	w.SetContent(container.NewVBox(
 		hello,
 		widget.NewButton("Hi!", func() {
@@ -18,5 +22,34 @@ func main() {
 		}),
 	))
 
-	w.ShowAndRun()
+	w.Show()
+
+	go func() {
+		for range time.Tick(time.Second) {
+			updateTime(hello)
+		}
+	}()
+
+	// second window
+	w2 := a.NewWindow("Larger")
+	w2.SetContent(widget.NewButton("Open new", func() {
+		w3 := a.NewWindow("Third")
+		w3.SetContent(widget.NewLabel("Third"))
+		w3.Show()
+	}))
+	w2.Resize(fyne.NewSize(100, 100))
+	w2.Show()
+
+	a.Run()
+
+	tidyUp()
+}
+
+func tidyUp() {
+	fmt.Println("Exited")
+}
+
+func updateTime(clock *widget.Label) {
+	formatted := time.Now().Format("Time: 03:04:05")
+	clock.SetText(formatted)
 }
